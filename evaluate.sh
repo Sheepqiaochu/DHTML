716562c04838e391863a3912fc94a647879bfc89
yang_whu <1597516368@qq.com>
2021-03-12 16:36:46 +0800

    save eva

--- evaluate.sh
+++ evaluate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 epoch=20
-while(($epoch<2020))
+while(($epoch<480))
 do 
-    path="/data/users/yangqiancheng/logs/lbp1_sigma1000/models/epoch_"$epoch".pth.tar"
+    path="/data/users/yangqiancheng/logs/lbp_stage0_sigma1000/models/epoch_"$epoch".pth.tar"
     echo $path
-    epoch=`expr 100 + $epoch`
+    epoch=`expr 40 + $epoch`
     CUDA_VISIBLE_DEVICES=4  python main.py --evaluate $path  --batch_size 128 --arch ShuffleNetV2 --width_mul 0.5
 done       
 
